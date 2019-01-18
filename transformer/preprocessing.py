880c3acd3ef39f34e351e6a4812a8e0a97c40946
Stefan Pantic <stefanpantic13@gmail.com>
2019-01-18 21:08:13 +0100

    Fixed a bug with indexing, moved file

--- transformer/preprocessing.py
+++ transformer/preprocessing.py
@@ -19,18 +19,25 @@ def make_vocabulary(infile, outfile):
         Output file name.
     """
     # Read raw data from infile
+    if os.path.exists(outfile):
+        return
+
     with codecs.open(infile, 'r', 'utf-8') as f:
         text = regex.sub('[^\s\p{Latin}\']', '', f.read())
 
     # Count words in corpora
     word_counts = Counter(text.split())
-    if not os.path.exists('preprocessed'):
-        os.mkdir('preprocessed')
+
+    # Create nested directory
+    if not os.path.exists(os.path.dirname(outfile)):
+        os.makedirs(os.path.dirname(outfile))
 
     # Write sorted word index to outfile
-    with codecs.open(f'./preprocessed/{outfile}', 'w', 'utf-8') as f:
+    with codecs.open(outfile, 'w', 'utf-8') as f:
         writer = csv.DictWriter(f, ['TOKEN', 'FREQUENCY'])
         writer.writeheader()
+        writer.writerows(
+            [{'TOKEN': token, 'FREQUENCY': 10000000} for token in ['<PAD>', '<UNK>', '<S>', '</S>']])
         for word, count in word_counts.most_common(len(word_counts)):
             writer.writerow({'TOKEN': word, 'FREQUENCY': count})
 
@@ -51,7 +58,7 @@ def load_vocabulary(infile, min_occurrences=50):
     """
     with codecs.open(infile, 'r', 'utf-8') as f:
         reader = csv.DictReader(f)
-        words = [row['TOKEN'] for row in reader if row['FREQUENCY'] >= min_occurrences]
+        words = [row['TOKEN'] for row in reader if int(row['FREQUENCY']) >= min_occurrences]
 
     indexed_words = {word: index for index, word in enumerate(words)}
     worded_indexes = {index: word for word, index in indexed_words.items()}
@@ -81,22 +88,22 @@ def create_data(source_sentences, target_sentences, en_vocab_file, de_vocab_file
     """
     en_wti, en_itw = load_vocabulary(en_vocab_file)
     de_wti, de_itw = load_vocabulary(de_vocab_file)
-    x_data, y_data, source, target = [], [], [], []
+    x_data, y_data, sources, targets = [], [], [], []
 
-    for sentence, target in zip(source_sentences, target_sentences):
-        x = [en_wti.get(word, 1) for word in sentence.split()] + [en_wti['</S>']]
+    for source, target in zip(source_sentences, target_sentences):
+        x = [en_wti.get(word, 1) for word in source.split()] + [en_wti['</S>']]
         y = [de_wti.get(word, 1) for word in target.split()] + [de_wti['</S>']]
 
         if max(len(x), len(y)) < maxlen:
             x_data.append(x)
             y_data.append(y)
-            source.append(sentence)
-            target.append(target)
+            sources.append(source)
+            targets.append(target)
 
     x_data = pad_sequences(x_data, maxlen=maxlen, padding='post')
     y_data = pad_sequences(y_data, maxlen=maxlen, padding='post')
 
-    return x_data, y_data, source, target
+    return x_data, y_data, sources, targets
 
 
 def load_data(input_file, label_file, en_vocab_file, de_vocab_file, sentence_fn):
@@ -126,7 +133,8 @@ def load_data(input_file, label_file, en_vocab_file, de_vocab_file, sentence_fn)
 
 def create_train_sentences(infile):
     """Creates sentences from data in passed file."""
-    return [regex.sub('[^\s\p{Latin}\']', '', line) for line in codecs.open(infile, 'r', 'utf-8').read().split('\n') if
+    return [regex.sub('[^\s\p{Latin}\']', '', line) for line in
+            codecs.open(infile, 'r', 'utf-8').read().split('\n') if
             line and not line.startswith('<')]
 
 
@@ -153,10 +161,10 @@ def create_datasets(flags):
         train_dataset, test_dataset: Iterators for train and test datasets.
     """
     # Load data
-    x_train, y_train, *_ = load_data(flags.train_inputs, flags.train_labels, flags.en_vocab_file, flags.de_vocab_file,
-                                     create_train_sentences)
-    x_test, y_test, *_ = load_data(flags.test_inputs, flags.test_labels, flags.en_vocab_file, flags.de_vocab_file,
-                                   create_test_sentences)
+    x_train, y_train, *_ = load_data(flags.train_inputs, flags.train_labels, flags.en_vocab_path,
+                                     flags.de_vocab_path, create_train_sentences)
+    x_test, y_test, *_ = load_data(flags.test_inputs, flags.test_labels, flags.en_vocab_path,
+                                   flags.de_vocab_path, create_test_sentences)
 
     train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
     test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
